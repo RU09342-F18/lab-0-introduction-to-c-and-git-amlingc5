d7444a26939cf58144aebdf1878e34ee95506ed9
amlingc5 <35497814+amlingc5@users.noreply.github.com>
2018-09-10 23:37:33 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,1 +1,57 @@
+#include <math.h>
 
+
+      int math(int num1, int num2, char operator) //initializes the two numbers and the operator
+      {
+      switch(operator)
+      {
+          //Addition case
+        case '+':
+          return num1 + num2;
+          
+             //Subtraction case
+        case '-':
+          return num1 - num2;
+          
+             //Multiplication case
+        case '*':
+          return num1 * num2;
+          
+             //Division case
+        case '/':
+          return num1 / num2;
+          
+             //modulus case
+        case '%':
+          return num1 % num2;
+          
+             //left shift case
+        case '<':
+          return num1 << num2;
+          
+             //right shift case
+        case '<':
+          return num1 >> num2;
+           
+             //bitwise and case
+        case '&':
+          return num1 & num2;
+           
+             //bitwise or case
+        case '|':
+          return num1 | num2;
+              
+             //bitwise xor case
+        case '^':
+          return num1 ^ num2;
+             
+             //inverse case
+        case '~':
+          return ~num1, ~num2;
+          
+        default:
+          return 0;
+          
+      }
+}
+          
